6a03cabb854d6fda70d8c26af916996da335152b
Aladin Taleb <aladin.taleb@gmail.com>
2020-07-12 23:50:26 +0200

    [fix] contact db

--- app/db/db.js
+++ app/db/db.js
@@ -1,38 +1,38 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 class FakeDatabase {
-    constructor() {
-        this._contacts = {};
+  constructor() {
+    this._contacts = {};
+  }
+
+  async getContacts() {
+    return Object.values(this._contacts).sort();
+  }
+
+  async getContact(contactId) {
+    return this._contacts[contactId];
+  }
+
+  async createContact(contactInfos) {
+    const contactId = uuid();
+    const contact = { ...contactInfos, id: contactId };
+    this._contacts[contactId] = contact;
+    return contact;
+  }
+
+  async deleteContact(contactId) {
+    delete this._contacts[contactId];
+  }
+
+  async updateContact(contactId, contactInfos) {
+    if (this._contacts[contactId] == undefined) {
+      return;
     }
 
-    async getContacts() {
-        return Object.values(this._contacts);
-    }
+    this._contacts[contactId] = { ...contactInfos, id: contactId };
 
-    async getContact(contactId) {
-        return this._contacts[contactId]
-    }
-
-    async createContact(contactInfos) {
-        const contactId = uuid();
-        const contact = {...contactInfos, id: contactId};
-        this._contacts[contactId] = contact;
-        return contact;
-    }
-
-    async deleteContact(contactId) {
-        delete this._contacts[contactId];
-    }
-
-    async updateContact(contactId, contactInfos) {
-        if (this._contacts[contactId] == undefined) {
-            return;
-        }
-
-        this._contacts[contactId] = contactInfos;
-        return contactInfos;
-    }
-
+    return this._contacts[contactId];
+  }
 }
 
 const Database = new FakeDatabase();
